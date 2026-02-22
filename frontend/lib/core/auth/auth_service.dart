9c3d0615431d77a7463bcc9eede00f6607574581
originalhawk242 <oghawk242@gmail.com>
2026-02-22 13:04:25 -0800

    feat(auth): add Google Sign-In serverClientId for Windows desktop

--- frontend/lib/core/auth/auth_service.dart
+++ frontend/lib/core/auth/auth_service.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import '../auth/auth_exceptions.dart';
-import '../../data/models/user_profile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'auth_exceptions.dart';
+import '../../data/models/user_profile.dart';
 
 class AuthService {
   final FirebaseAuth _auth;
@@ -14,7 +14,9 @@ class AuthService {
     GoogleSignIn? googleSignIn,
     FirebaseFirestore? firestore,
   })  : _auth = auth ?? FirebaseAuth.instance,
-        _googleSignIn = googleSignIn ?? GoogleSignIn(),
+        _googleSignIn = googleSignIn ?? GoogleSignIn(
+          serverClientId: '80003441956-omm6u1f2krdqrds38an85dor4q0p4sal.apps.googleusercontent.com',
+        ),
         _firestore = firestore ?? FirebaseFirestore.instance;
 
   Stream<User?> get authStateChanges => _auth.authStateChanges();
