6ee782a49dae2cc444e44ca41640cc75d72026f1
originalhawk242 <oghawk242@gmail.com>
2026-02-28 19:59:03 -0800

    chore(functions): disable TBA integration temporarily

--- functions/main.py
+++ functions/main.py
@@ -7,8 +7,8 @@ from firebase_admin import initialize_app, firestore
 initialize_app()
 db = firestore.client()
 
-# Import existing TBA function
-from .tba_sync import fetch_event_schedule
+# TBA function - commented out for now (TBA integration not ready)
+# from .tba_sync import fetch_event_schedule
 
 # Import Firestore triggers
 from .triggers.match_sync import on_match_created, on_match_updated
@@ -18,7 +18,7 @@ from .handlers.backfill import backfill_event_to_sheets
 
 # Re-export all functions
 __all__ = [
-    'fetch_event_schedule',
+    # 'fetch_event_schedule',  # Disabled - TBA integration paused
     'on_match_created',
     'on_match_updated',
     'backfill_event_to_sheets'
